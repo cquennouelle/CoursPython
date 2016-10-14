f0699f41cae9e1e8e756a92308c5375c4b0df9bd
Cyril Quennouelle <cyril.quennouelle@basystemes.fr>
2016-10-14 21:45:49 +0200

    Construction of a Maze from a string.

---
+++ mazeMap.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 14 20:15:18 2016
+
+@author: localuser
+"""
+
+class MazeMap:
+    """Class representing a map."""
+    
+    def LoadFromString(self, string):
+        size = len(string)
+        self.height = 0
+        self.width = 0
+        self.grid = []
+        if size > 0:
+            rows = string.split(sep='\n')
+            self.height = 0
+            for row in rows:
+                gridRow = []
+                for letter in row:
+                    gridRow.append(letter)
+                self.grid.append(gridRow)
+            self.height = len(self.grid)
+            self.width = len(rows)
+    
+    def __init__(self, string=''):
+        if type(string) is not str:
+            raise TypeError("Requires a string to build a maze.")
+        if string == '':
+            self.height = 0
+            self.width = 0
+            self.grid = []
+        else:
+            self.LoadFromString(string)
+    
+    def __repr__(self):
+        return "<Map {}x{}>".format(self.height, self.width)
+    
---
+++ test_map.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 11 11:29:14 2016
+
+@author: cquennouelle
+"""
+
+import unittest
+from mazeMap import *
+
+class MapTest(unittest.TestCase):
+    """Test class Map."""
+    
+    def testInitWithParametersError(self):
+        """Test construction error when wrong parameter type."""
+        with self.assertRaises(TypeError):
+            myMap = MazeMap(1)
+
+    def testInit(self):
+        """Test for empty construction."""
+        myMap = MazeMap()
+        self.assertEqual(myMap.height, 0)
+        self.assertEqual(myMap.width, 0)
+        self.assertEqual(myMap.grid, [])
+
+    def testInit2(self):
+        """Test construction from an empty string."""
+        myMap = MazeMap('')
+        self.assertEqual(myMap.height, 0)
+        self.assertEqual(myMap.width, 0)
+        self.assertEqual(myMap.grid, [])
+            
+    def testInit3(self):
+        """Test construction from a string."""
+        myMap = MazeMap('OOO\nO U\nOOO')
+        self.assertEqual(myMap.height, 3)
+        self.assertEqual(myMap.width, 3)
+        self.assertEqual(myMap.grid, [['O', 'O', 'O'], ['O', ' ', 'U'], ['O', 'O', 'O']])
+         
